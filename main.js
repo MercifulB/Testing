1ac6c1f0b646b8cad71b99af1b51446e5257a2d7
MercifulB <112597893+MercifulB@users.noreply.github.com>
2023-02-18 16:38:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import './style.css';
+import './cards.css';
+
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
