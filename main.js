fc67dac7652747bbd9ebe5f9f4cf1ab52ebae0be
MercifulB <112597893+MercifulB@users.noreply.github.com>
2023-02-18 16:12:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,203 +1,130 @@
-import * as THREE from 'three'
-import './style.css'
-import gsap from 'gsap'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+// Setup
 
-//* ---------Basic setup-------------
+const scene = new THREE.Scene();
 
-const scene = new THREE.Scene()
-
-//! Sizes  Really...!!!  Howaaaaaaaiiiiiiiiiiiiiii..........
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-
-const camera = new THREE.PerspectiveCamera(40, sizes.width / sizes.height, 0.01, 2000)
-
-camera.position.set(10, 0, 28)
-camera.rotation.set(0, 15, 0)
-
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('.webgl'),
-    preserveDrawingBuffer: true
-})
-
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.setPixelRatio(window.devicePixelRatio)
-
-
-//^ -------------------------------------------
-//* -------------ScreenShot--------------------
-
-
-document.querySelector('.capture').addEventListener('click', saveAsImage)
-
-function saveAsImage() {
-    
-        let strMime = "image/jpeg"
-        let imgData = renderer.domElement.toDataURL(strMime)
-
-        saveFile(imgData.replace(strMime), "P L A N E T.jpg")
-}
-
-let saveFile = function (strData, filename) {
-
-    let link = document.createElement('a')
-
-    if (typeof link.download === 'string') {
-
-        document.body.appendChild(link); //!Firefox requires the link to be in the body
-        link.download = filename
-        link.href = strData
-        link.click();
-        document.body.removeChild(link); //!remove the link when done
-    }
-    else {
-        location.replace(uri);
-    }
-}
-
-//? https://codepen.io/shivasaxena/pen/QEzrrv
-//^ -------------------------------------------
-
-
-
-//* ------------Controller----------------
-
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
-// controls.enablePan = false
-// controls.enableZoom = false
-controls.autoRotate = true
-controls.autoRotateSpeed = 0.2
-
-
-//* -----------Responsive-------------------
-
-window.addEventListener('resize', () => {
-
-    //^ Update Sizes ----
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    //^ Update Camera ----
-    camera.updateProjectionMatrix()
-    camera.aspect = sizes.width / sizes.height
-    renderer.setSize(sizes.width, sizes.height)
-})
-
-
-//* ------------Lights-----------------
-
-const pointLit = new THREE.PointLight(0xffffff)
-pointLit.position.set(15, 15, 15)
-const ambLight = new THREE.AmbientLight(0xffffff)
-ambLight.intensity = 0.05
-scene.add(pointLit, ambLight)
-
-// scene.background = new THREE.TextureLoader().load('public/stary_skyTexture.jpg')
-
-
-//* ------------Materials----------------
-
-const planetTexture = new THREE.TextureLoader().load('/Jupiter_Map.jpg')
-const ringTexture = new THREE.TextureLoader().load('/2k_jupiter.jpg')
-
-
-//* -----------3D Objects--------------
-
-const planet = new THREE.Mesh(
-    new THREE.SphereGeometry(3, 80, 64),
-    new THREE.MeshStandardMaterial({
-        map: planetTexture
-    })
-)
-planet.rotation.y = 2
-scene.add(planet)
-
-
-const geometry = new THREE.TorusGeometry(4, 0.3, 2, 100)
-const material = new THREE.MeshBasicMaterial({ map: ringTexture })
-const ring = new THREE.Mesh(geometry, material)
-ring.rotation.x = 2
-ring.rotation.y = 3
-scene.add(ring)
-
-
-function addStar() {
-    const geometry = new THREE.SphereGeometry(0.3, 10, 10)
-    const material = new THREE.MeshBasicMaterial({ color: '#ffffff' })
-    const star = new THREE.Mesh(geometry, material)
-
-    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(2000))
-
-    star.position.set(x, y, z)
-    scene.add(star)
-}
-
-Array(10000).fill().forEach(addStar)
-
-//* -------------------------------------
-
-
-const listener = new THREE.AudioListener();
-camera.add(listener)
-
-const sound = new THREE.Audio( listener );
-
-const audioLoader = new THREE.AudioLoader();
-audioLoader.load( 'sounds/SpaceAmbientmusic.ogg', 
-(buffer)=>{
-    sound.setBuffer(buffer)
-    sound.setVolume(0.05)
-    sound.setLoop(true)
-    sound.play()
+  canvas: document.querySelector('#bg'),
 });
 
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
 
-//* -----------Helpers-------------------
+renderer.render(scene, camera);
 
-// const lightHelper = new THREE.PointLightHelper(pointLit)
-// const gridHelper = new THREE.GridHelper(200, 50)
+// Torus
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+// Helpers
+
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200, 50);
 // scene.add(lightHelper, gridHelper)
 
+// const controls = new OrbitControls(camera, renderer.domElement);
 
-//* ---------Animation-------------------
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
 
-const animPlanet = gsap.timeline({ defaults: { duration: 10 } })
-animPlanet.delay(4).timeScale(0.5).fromTo(planet.scale, { z: 6, x: 6, y: 6 }, { z: 1, x: 1, y: 1 })
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
 
-
-const animRing = gsap.timeline({ defaults: { duration: 10 } })
-animRing.delay(4).timeScale(0.5).fromTo(ring.scale, { z: 6, x: 6, y: 6 }, { z: 1, x: 1, y: 1 })
-
-
-const animTxt = gsap.timeline({ defaults: { duration: 3 } })
-animTxt.fromTo('.header', { y: '-100%' }, { y: '0%' })
-animTxt.fromTo('.footer', { y: '70%' }, { y: '0%' })
-
-
-const opacity = gsap.timeline({ defaults: { duration: 2 } })
-opacity.delay(1.2).timeScale(0.5).fromTo('.opacity', { opacity: 0 }, { opacity: 1 })
-
-const opacity2 = gsap.timeline({ defaults: { duration: 1 } })
-opacity2.delay(0.5).timeScale(0.5).fromTo('.header', { opacity: 0 }, { opacity: 1 })
-opacity2.delay(0.5).timeScale(0.5).fromTo('.footer', { opacity: 0 }, { opacity: 1 })
-
-
-//* ----------Recursion------------------
-
-function loop() {
-    controls.update()
-
-    ring.rotation.z += 0.01
-    planet.rotation.y -= 0.002
-
-
-    renderer.render(scene, camera)
-    requestAnimationFrame(loop)
+  star.position.set(x, y, z);
+  scene.add(star);
 }
-loop()
+
+Array(200).fill().forEach(addStar);
+
+// Background
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
+
+// Avatar
+
+const avatarTexture = new THREE.TextureLoader().load('avatar.jpeg');
+
+const avatar = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: avatarTexture }));
+
+scene.add(avatar);
+
+// Moon
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture,
+  })
+);
+
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+avatar.position.z = -5;
+avatar.position.x = 2;
+
+// Scroll Animation
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  avatar.rotation.y += 0.01;
+  avatar.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+// Animation Loop
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  moon.rotation.x += 0.005;
+
+  // controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
